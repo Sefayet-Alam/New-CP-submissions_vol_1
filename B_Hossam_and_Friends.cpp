da2b1b3893515ea96feb8ed668ec276c08e5213a
Sefayet_014 <sefayetalam14@gmail.com>
2023-01-06 23:22:07 +0600

    rectified

---
+++ B_Hossam_and_Friends.cpp
@@ -0,0 +1,124 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+#define ll                  long long
+#define scl(n)              scanf("%lld", &n)
+#define fr(i,n)             for (ll i=0;i<n;i++)
+#define fr1(i,n)            for(ll i=1;i<=n;i++)
+#define pfl(x)              printf("%lld\n",x)
+#define pb                  push_back
+#define asort(a)            sort(a,a+n)
+#define dsort(a)            sort(a,a+n,greater<int>())
+#define vasort(v)         sort(v.begin(), v.end());
+#define vdsort(v)         sort(v.begin(), v.end(),greater<ll>());
+#define pn                  printf("\n")
+#define md                  10000007
+#define debug               printf("I am here\n")
+
+#define l(s)                      s.size()
+#define tcas(i,t)             for(ll i=1;i<=t;i++)
+#define pcas(i)                printf("Case %lld: ",i)
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define Setpre(n) cout<<fixed<<setprecision(n)
+inline ll GCD(ll a, ll b) { return b == 0 ? a : GCD(b, a % b); }
+inline ll LCM(ll a, ll b) { return a * b / GCD(a, b); }
+inline ll Ceil(ll p, ll q)  {return p < 0 ? p / q : p / q + !!(p % q);}
+inline ll Floor(ll p, ll q) {return p > 0 ? p / q : p / q - !!(p % q);}
+inline double logb(ll base,ll num){ return (double)log(num)/(double)log(base);}
+#define M 10000
+
+
+inline bool isPerfectSquare(long double x){ if (x >= 0) { long long sr = sqrt(x);return (sr * sr == x); }return false; }
+double euclidean_distance(ll x1,ll y1,ll x2,ll y2){double a=(x2-x1)*(x2-x1);double b=(y2-y1)*(y2-y1);double c=(double)sqrt(a+b);return c;}
+int popcount(ll x){return __builtin_popcountll(x);};
+int poplow(ll x){return __builtin_ctzll(x);};
+int pophigh(ll x){return 63 - __builtin_clzll(x);};
+
+
+
+/*===================================================================//
+            
+        ░█▀▀▀█ ░█▀▀▀ ░█▀▀▀ ─█▀▀█ ░█──░█ ░█▀▀▀ ▀▀█▀▀ 
+        ─▀▀▀▄▄ ░█▀▀▀ ░█▀▀▀ ░█▄▄█ ░█▄▄▄█ ░█▀▀▀ ─░█── 
+        ░█▄▄▄█ ░█▄▄▄ ░█─── ░█─░█ ──░█── ░█▄▄▄ ─░█──
+//====================================================================*/
+
+void setIO(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+
+    freopen("output.txt", "w", stdout);
+    #endif // ONLINE_JUDGE
+
+}
+
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+int main()
+{
+    fast;
+     ll t;
+    //setIO();
+     //ll tno=1;;
+     //t=1;
+    cin>>t;
+
+    while(t--){
+        ll n,m;
+        cin>>n>>m;
+        ll tot=(n*(n+1))/2;
+        map<ll,ll>mp;
+        for(ll i=1;i<=n;i++){
+            mp[i]=INT_MAX;
+        }
+        vector<ll>g[n+1];
+         ll x,y,minm,maxm;
+        while(m--){
+           
+            cin>>x>>y;
+           if(x>y) swap(x,y);
+            mp[x]=min(mp[x],y);
+        }
+        bool f=0;
+        ll minpos=0;
+        ll maxpos=n;
+        ll r=mp[n];
+        for(ll i=n-1;i>=1;i--){
+            if(mp[i]!=INT_MAX){
+                mp[i]=min(mp[i],r);
+                r=mp[i];
+            }
+        }
+        for(auto it:mp){
+            if(it.second!=INT_MAX){
+                //cout<<it.first<<" "<<it.second<<" "<<minpos<<endl; 
+                tot-=(n-it.second+1);
+                tot-=(it.first-minpos-1)*(n-it.second+1);
+                minpos=it.first;
+                maxpos=it.second;
+            }
+        }
+   
+        cout<<tot<<endl;
+
+    }
+
+
+    return 0;
+}
